9ea19f8053db1bdb6b91a493a77b57e7f15e2888
C.Bright <111628069+chahyeoncheol@users.noreply.github.com>
2023-07-22 13:24:21 +0900

    [Bronze I] Title: 단어 공부, Time: 220 ms, Memory: 33212 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,13 +1,15 @@
-a = input().lower()
-a_d = {i:0 for i in a}
+a = input().upper()
+a_d = {}
 
 for i in a:
     if i in a_d:
         a_d[i] = a_d[i] + 1
+    else:
+        a_d[i] = 1
 
 result = [k for k, v in a_d.items() if v == max(a_d.values())]
 
 if len(result) >= 2:
     print('?')
 else:
-    print(result[0].upper())
+    print(result[0])
