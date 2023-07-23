575822995d64df97977ca6dca490afa42ead5df6
C.Bright <111628069+chahyeoncheol@users.noreply.github.com>
2023-07-23 21:06:40 +0900

    [Bronze I] Title: 진법 변환 2, Time: 120 ms, Memory: 34972 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
@@ -0,0 +1,12 @@
+import string
+
+def convert_recur(num, base):
+    number = string.digits + string.ascii_uppercase
+    q, r = divmod(num, base)
+    return convert_recur(q, base) + number[r] if q else number[r]
+
+
+num, base = map(int, input().split())
+
+
+print(convert_recur(num, base))
