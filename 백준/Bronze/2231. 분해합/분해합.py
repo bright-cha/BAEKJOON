0cc26f00c52ece8ffebd285489990a155efdcaea
C.Bright <111628069+chahyeoncheol@users.noreply.github.com>
2023-07-30 21:06:32 +0900

    [Bronze II] Title: 분해합, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -4,12 +4,16 @@ input = sys.stdin.readline
 n = int(input())
 
 res = []
-for i in range(n):
-    sum_num = 0
-    for j in str(i):
-        sum_num += int(j)
-    if i + sum_num == n:
-        res.append(i)
+for i in range((len(str(n)) * 9) + 1):
+    if i < n:
+        origin = n - i
+    
+    num = 0
+    for j in str(origin):
+        num += int(j)
+    
+    if origin + num == n:
+        res.append(origin)
 
 if res == []:
     print(0)
