b993be502b95508a67193750771649393ed3bccb
C.Bright <111628069+chahyeoncheol@users.noreply.github.com>
2023-08-31 23:24:22 +0900

    [Silver II] Title: 섬의 개수, Time: 64 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,28 +1,44 @@
-while True:
-    col, row = map(int, input().split())
-    if col == 0 and row == 0:
-        break
-    matrix = [list(map(int, input().split())) for _ in range(row)]
+import sys
 
-    di = [0, 0, 1, 1, 1, -1, -1, -1]
-    dj = [1, -1, 0, 1, -1, 0, 1, -1]
-    cnt = 0
-    for i in range(row):
-        for j in range(col):
-            if matrix[i][j] == 1:
-                stack = [(i, j), (i, j)]
-                matrix[i][j] = 0
-                while stack:
-                    for k in range(8):
-                        ni, nj = i + di[k], j + dj[k]
-                        if 0 <= ni < row and 0 <= nj < col:
-                            if matrix[ni][nj] == 1:
-                                stack.append((i, j))
-                                matrix[ni][nj] = 0
-                                i = ni
-                                j = nj
-                                break
-                    else:
-                        i, j = stack.pop()
-                cnt += 1
-    print(cnt)
+input = sys.stdin.readline
+
+
+def dfs(x, y):
+    stack = []
+    stack.append((x, y))
+
+    while stack:
+        for i in range(8):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < h and 0 <= ny < w and arr[nx][ny] == '1':
+                stack.append((x, y))
+                arr[nx][ny] = '0'
+                x = nx
+                y = ny
+                break
+        else:
+            x, y = stack.pop()
+
+    return 1
+
+
+dx = [-1, -1, -1, 0, 0, 1, 1, 1]
+dy = [-1, 0, 1, -1, 1, -1, 0, 1]
+
+in_, put_ = map(int, input().rstrip().split())
+
+while in_ != 0 and put_ != 0:
+    w, h = in_, put_
+    arr = [input().rstrip().split() for _ in range(h)]
+    ans = 0
+
+    for x in range(h):
+        for y in range(w):
+            if arr[x][y] == '1':
+                arr[x][y] == '0'
+                ans += dfs(x, y)
+
+
+    print(ans)
+    in_, put_ = map(int, input().rstrip().split())
