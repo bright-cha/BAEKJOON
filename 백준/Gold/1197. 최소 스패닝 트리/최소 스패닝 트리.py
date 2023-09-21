b44f7c22f8728f3cdf60b7f310624da2caa182d0
C.Bright <111628069+chahyeoncheol@users.noreply.github.com>
2023-09-21 21:21:57 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 4348 ms, Memory: 49860 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -0,0 +1,40 @@
+import heapq
+def find_set(x):
+    if parents[x] == x:
+        return x
+
+    parents[x] = find_set(parents[x])
+    return parents[x]
+
+
+def union(x, y):
+    x = find_set(x)
+    y = find_set(y)
+
+    if x < y:
+        parents[y] = x
+    else:
+        parents[x] = y
+
+
+V, E = map(int, input().split())
+
+edge = []
+for _ in range(E):
+    f, t, w = map(int, input().split())
+    edge.append((w, f - 1, t - 1))
+
+edge.sort()
+
+parents = [i for i in range(V)]
+
+cnt, rst = 0, 0
+for w, f, t in edge:
+    if find_set(f) != find_set(t):
+        cnt += 1
+        rst += w
+        union(f, t)
+        if cnt == V:
+            break
+
+print(rst)
