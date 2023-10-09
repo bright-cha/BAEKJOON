8baf896e0e3c09be0f9c26a8d1eb58f43d81f303
C.Bright <111628069+chahyeoncheol@users.noreply.github.com>
2023-10-10 00:20:37 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -1,33 +1,31 @@
 import sys
 input = sys.stdin.readline
-def dfs(x, y, direc):
-    global cnt
-    if cnt >= 1000000:
-        return
 
-    if x == size - 1 and y == size - 1:
-        cnt += 1
-        return
-
-    # 대각선인 경우
-    if x + 1 < size and y + 1 < size:
-        if matrix[x + 1][y + 1] == 0 and matrix[x + 1][y] == 0 and matrix[x][y + 1] == 0:
-            dfs(x + 1, y + 1, 3)
-
-    # 세로인 경우
-    if x + 1 < size and (direc == 2 or direc == 3):
-        if matrix[x + 1][y] == 0:
-            dfs(x + 1, y, 2)
-
-    # 가로인 경우
-    if y + 1 < size and (direc == 1 or direc == 3):
-        if matrix[x][y + 1] == 0:
-            dfs(x, y + 1, 1)
-            
-            
 size = int(input())
 matrix = [list(map(int, input().split())) for _ in range(size)]
+dp = [[[0] * 3 for _ in range(size)] for _ in range(size)] # 0대각선 1 가로 2세로
+# 출발 세팅
+dp[0][1][1] = 1
+# 첫 줄, 0행은 출발지의 가로 상태에서만 이동 가능하니 기록
+for i in range(2, size):
+    if matrix[0][i] == 0:
+        dp[0][i][1] = dp[0][i - 1][1]
 
-cnt = 0
-dfs(0, 1, 1)
-print(cnt)
+for i in range(1, size):
+    for j in range(1, size):
+        # 이동이 가능한 경우
+        if matrix[i][j] == 0:
+            # ij의 대각선이 가능한 경우
+            if matrix[i][j - 1] == 0 and matrix[i - 1][j] == 0:
+                # 꼬리부분 도착 가능 경우 합
+                dp[i][j][0] = dp[i - 1][j - 1][0] + dp[i - 1][j - 1][2] + dp[i - 1][j - 1][1]
+
+            # 가로 이동이 가능한 경우
+            # 꼬리 부분에서 가로로 이동 가능한 경우 합
+            dp[i][j][1] = dp[i][j - 1][0] + dp[i][j - 1][1]
+            # 세로 이동이 가능한 경우
+            # 꼬리 부분에서 세로로 이동 가능한 경우 합
+            dp[i][j][2] = dp[i - 1][j][0] + dp[i - 1][j][2]
+
+# 최종적으로 도착지의 모든 경우의수 합
+print(dp[size - 1][size - 1][0] + dp[size - 1][size - 1][1] + dp[size - 1][size - 1][2])
